c3f91cd0f5352dd7035c1d4966ce872b21bafe0d
David Gomez <dgomez@iconcologia.net>
2018-10-03 14:45:15 +0200

    Fix BasicValidator

--- infocentre_data_manager/plugins/data_validators/basic.py
+++ infocentre_data_manager/plugins/data_validators/basic.py
@@ -24,14 +24,14 @@ class BasicValidator(DataValidator):
         self.warnings = []
         self.errors = []
 
-        self._check_num_vars(data_dict)
-        self._check_order_vars(data_dict)
+        self._check_vars_match(data_dict)
+        self._check_general_row(data_dict)
         self._check_table_content_description(data_dict)
         self._check_no_description_vars(data_dict)
         self._check_invalid_vars(data_dict)
 
-        if len(self.errors) == 0:
-            self.info.append('No errors found')
+        if len(self.errors) + len(self.warnings) == 0:
+            self.info.append('No problems found.')
 
         return {
             'info': self.info,
@@ -39,28 +39,25 @@ class BasicValidator(DataValidator):
             'errors': self.errors
         }
 
-    def _check_num_vars(self, data_dict):
-        if len(data_dict['variables']['variable']) != \
-           len(data_dict['data'].columns):
-            self.errors.append(
-                'The number of variables in the definition is different '
-                'than in the data.'
-            )
-
-    def _check_order_vars(data_dict):
+    def _check_vars_match(self, data_dict):
         var_names = data_dict['variables']['variable']
         data_var_names = data_dict['data'].columns
-        if set(var_names) = set(data_var_names) and \
-                var_names != data_var_names:
-            self.warnings.append(
-               'Variables as defined are in different order than in the data.'
+        if list(var_names) != list(data_var_names):
+            self.errors.append(
+               "Variable names don't match the data columns."
             )
 
-    def _check_table_content_description(data_dict):
-        if data_dict['general']['contents'] == '':
-            self.warnings.append('Data table content description is empty')
+    def _check_general_row(self, data_dict):
+        if len(data_dict['general']) == 0:
+            self.errors.append('Data table general info is missing.')
+        elif len(data_dict['general']) > 1:
+            self.errors.append('More than one row of general info.')
 
-    def _check_no_description_vars(data_dict):
+    def _check_table_content_description(self, data_dict):
+        if data_dict['general']['contents'].iloc[0] == '':
+            self.warnings.append('Data table content description is empty.')
+
+    def _check_no_description_vars(self, data_dict):
         no_description_vars = [var.variable
                                for var in data_dict['variables'].itertuples()
                                if var.description == '']
@@ -68,19 +65,19 @@ class BasicValidator(DataValidator):
         if len(no_description_vars) > 0:
             self.warnings.append(
                 'The variable(s) {} have no description.'.format(
-                    no_description_vars
+                    ', '.join(no_description_vars)
                 ))
 
-    def _check_invalid_vars(data_dict):
+    def _check_invalid_vars(self, data_dict):
         invalid_var_names = [var.variable
                              for var in data_dict['variables'].itertuples()
-                             if bool(re.fullmatch(r'[a-zA-Z0-9_]+',
-                                     var.variable))]
+                             if not bool(re.fullmatch(r'[a-zA-Z0-9_]+',
+                                         var.variable))]
 
         if len(invalid_var_names) > 0:
             self.errors.append(
-                'The variable names {} are invalid, only names with '
+                'The variable name(s) {} are invalid, only names with '
                 'lowercase and uppercase letters, numbers and '
                 'underscores are allowed.'.format(
-                    invalid_var_names
+                    ', '.join(invalid_var_names)
                 ))
