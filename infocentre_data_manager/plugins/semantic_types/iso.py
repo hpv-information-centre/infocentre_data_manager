9685d2ca22f37c5fa0d0bfda0e95f6b967fa6e9a
David Gomez <dgomez@iconcologia.net>
2018-10-31 16:35:31 +0100

    Add ISO type validator

--- infocentre_data_manager/plugins/semantic_types/iso.py
+++ infocentre_data_manager/plugins/semantic_types/iso.py
@@ -6,6 +6,7 @@ This module includes the validator for variable typing.
 
 import logging
 import pymysql
+import pandas as pd
 from infocentre_data_manager.plugins.semantic_types.base import SemanticType
 
 __all__ = ['IsoType', ]
@@ -16,17 +17,17 @@ class IsoType(SemanticType):
     Plugin that implements the definition of ISO3 codes.
     """
 
-    def __init__(self, conn_settings):
-        conn = pymysql.connect(host=settings,
-                               user=user,
-                               password=password,
-                               db=db,
+    def __init__(self):
+        conn = pymysql.connect(read_default_file='/home/david/db.cnf',
+                               read_default_group='infocentre_epi',
                                charset='utf8',
                                cursorclass=pymysql.cursors.DictCursor)
+        self.available_isos = \
+            set(pd.read_sql('SELECT iso3Code '
+                            'FROM dict_regions', conn)['iso3Code'])
 
     def check(self, value, **kwargs):
-        # TODO
-        pass
+        return value in self.available_isos
 
     def help_info(self, **kwargs):
         return 'ISO3 codes'
