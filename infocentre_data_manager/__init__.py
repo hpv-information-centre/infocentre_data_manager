0f68b569e25cc8b07dadc7104feefd747ee166b6
David Gomez <dgomez@iconcologia.net>
2018-12-18 14:56:05 +0100

    Update version number

--- infocentre_data_manager/__init__.py
+++ infocentre_data_manager/__init__.py
@@ -1,5 +1,5 @@
 from infocentre_data_manager.plugins.codecs.base import Codec
 from infocentre_data_manager.plugins.data_validators.base import DataValidator
 
-__short_version__ = '0.1'
-__version__ = '0.1.0'
+__short_version__ = '0.2'
+__version__ = '0.2.0'
